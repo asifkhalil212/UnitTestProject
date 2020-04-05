0bc0a9451de8584c9e156309255ba6e28e3e8e93
mkhalil2 <mkhalil2@LHTU05CG9232JQX.ms.ds.uhc.com>
2020-04-06 02:58:26 +0530

    project commit to repository

---
+++ TestProject1/src/com/test/DatePrint.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import java.util.Date;
+
+public class DatePrint {
+
+	public static void main(String[] args) {
+		Date date = new Date();
+		System.out.println(date);
+	}
+
+}
